170a794c5fc96a8cd4986f75a8dda8689af22f26
Dev Ashish <ashishudupa282000@gmail.com>
2026-02-25 00:53:23 +0530

    car import

--- src/components/hero/Car.jsx
+++ src/components/hero/Car.jsx
@@ -1,12 +1,11 @@
-import carImage from "../../assets/car.png";
+import carImage from "/assets/car.png";
 
 export default function Car() {
   return (
     <img
       id="car"
       src={carImage}
-      alt="McLaren Car"
-      className="absolute top-0 left-0 h-[220px] z-10"
+   className="absolute top-0 left-0 h-[220px] z-10"
     />
   );
 }
